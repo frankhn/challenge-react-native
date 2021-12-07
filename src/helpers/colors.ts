52cc833e56235d5ecf7c275f73b7cd52155bd046
frankhn <harfrank2@gmail.com>
2021-12-07 21:49:19 +0200

    app default colors

---
+++ src/helpers/colors.ts
@@ -0,0 +1,9 @@
+
+export const primary = '#462055';
+export const grayOut = '#c6d8e8';
+export const white = '#ffffff';
+export const gray = '#ccc';
+export const yellow = '#FDCA40';
+export const yellowOut = '#3D2348';
+export const iceBlue = '#f6f9ff';
+export const darkGray = '#302e2e';
