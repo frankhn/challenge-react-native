d9279f92f3c0f038e5da82d1ec73e5ad098139ee
frankhn <harfrank2@gmail.com>
2021-12-07 21:49:35 +0200

    helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,17 @@
+import * as VALIDATIONS from './validations'
+import * as COLORS from './colors'
+import * as STRING from './string'
+import * as OBJECT from './Object'
+import * as METRIX from './metrix'
+import * as HTTP from './axios'
+import TOAST from './Toast'
+
+export {
+    VALIDATIONS,
+    COLORS,
+    STRING,
+    OBJECT,
+    METRIX,
+    HTTP,
+    TOAST
+}
