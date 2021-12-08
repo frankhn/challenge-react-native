97d8b259cb0bdbf9d4d492dffed0f719fff5e4e5
frankhn <harfrank2@gmail.com>
2021-12-08 23:31:23 +0200

    adds input bg color

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -1,5 +1,5 @@
 
-export const primary = '#462055';
+export const primary = '#3D2348';
 export const grayOut = '#c6d8e8';
 export const white = '#ffffff';
 export const gray = '#ccc';
@@ -7,3 +7,4 @@ export const yellow = '#FDCA40';
 export const yellowOut = '#3D2348';
 export const iceBlue = '#f6f9ff';
 export const darkGray = '#302e2e';
+export const primaryOut = '#462055'
