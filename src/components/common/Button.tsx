424982fae2fb61c774a10180891119743fff588c
frankhn <harfrank2@gmail.com>
2021-12-09 14:16:09 +0200

    adds a reusable button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,24 +3,29 @@ import {
     StyleProp,
     TouchableWithoutFeedback,
     Text,
+    View,
 } from 'react-native';
 import { COLORS, METRIX } from '../../helpers';
 interface IInput {
     handleChange(): void
     value?: any,
-    styles: StyleProp<any>
+    styles?: StyleProp<any>
+    textStyles?: StyleProp<any>
 }
 
 export default ({
     handleChange,
     styles,
     value,
+    textStyles
 }: IInput) => {
     return (
         <TouchableWithoutFeedback
-            onPress={handleChange}
-            style={[METRIX.style('mt-5 py-3 rounded-md'), styles]}>
-            <Text style={[METRIX.style('text-center text-yellow-400')]}>{value}</Text>
+            onPress={handleChange}>
+            <View
+                style={[METRIX.style('rounded-md py-3'), { backgroundColor: COLORS.yellow }]}>
+                <Text style={[METRIX.style('text-center')]}>{value}</Text>
+            </View>
         </TouchableWithoutFeedback>
     )
 }
