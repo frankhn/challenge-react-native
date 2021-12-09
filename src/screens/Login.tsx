16f043811d6c8af16995e5dd57849488211ed75e
frankhn <harfrank2@gmail.com>
2021-12-09 14:17:28 +0200

    implements login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,18 +1,48 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react'
 import { View } from 'react-native'
+import Button from '../components/common/Button';
+import Input from '../components/common/Input';
 import Layout from '../components/Layout';
+import {METRIX } from '../helpers';
 import { NavigatorParamList } from '../navigation/types';
 
 interface IHome {
     navigation: StackNavigationProp<NavigatorParamList, 'Home'>;
 }
 
-
 const Login = ({ navigation }: IHome) => {
     return (
         <Layout>
-
+            <>
+                <View style={
+                    METRIX.style('w-full py-6 px-4')}>
+                    <Input
+                        placeholder="Names"
+                        handleChange={() => null}
+                        returnKeyType='next'
+                        styles={
+                            METRIX.style('w-full')}
+                        value=''
+                        keyboardType='default'
+                    />
+                    <Input
+                        placeholder="Email"
+                        handleChange={() => null}
+                        returnKeyType='next'
+                        styles={
+                            METRIX.style('w-full')}
+                        value=''
+                        keyboardType='default'
+                    />
+                </View>
+                <View style={METRIX.style('w-full px-4 py-5')}>
+                    <Button
+                        handleChange={() => null}
+                        value={'Login'}
+                    />
+                </View>
+            </>
         </Layout>
     )
 }
