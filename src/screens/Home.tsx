0be59d1620225b4a0ff70e61415d220331d483a5
frankhn <harfrank2@gmail.com>
2021-12-09 14:17:19 +0200

    adds a home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,31 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react'
-import { View } from 'react-native'
+import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import Layout from '../components/Layout';
+import { METRIX, COLORS } from '../helpers';
 import { NavigatorParamList } from '../navigation/types';
 
 interface IHome {
     navigation: StackNavigationProp<NavigatorParamList, 'Home'>;
 }
 
-
 const Home = ({ navigation }: IHome) => {
     return (
-        <View>
-            
-        </View>
+        <Layout>
+            <View style={METRIX.style('flex-1 justify-end')}>
+                <View>
+                    <Text style={METRIX.style('self-center text-white')}>GARAGE</Text>
+                    <Text style={METRIX.style('self-center text-gray-400')}>some information here</Text>
+                </View>
+            </View>
+            <View style={METRIX.style('flex-1 justify-end px-4 pb-10')}>
+                <TouchableWithoutFeedback
+                    onPress={() => null}
+                    style={[METRIX.style('py-3 rounded-sm'), { backgroundColor: COLORS.yellow }]}>
+                    <Text style={METRIX.style('text-center')}>Login</Text>
+                </TouchableWithoutFeedback>
+            </View>
+        </Layout>
     )
 }
 
