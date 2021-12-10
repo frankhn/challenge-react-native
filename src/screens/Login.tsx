80e6c0fe552a3dac59d1aee4bccfff3af7e5fbb8
frankhn <harfrank2@gmail.com>
2021-12-10 10:54:42 +0200

    implements login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,52 +1,74 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
-import auth from '@react-native-firebase/auth';
-
 // Local import
 import Button from '../components/common/Button';
 import Input from '../components/common/Input';
 import Layout from '../components/Layout';
-import { METRIX, OBJECT } from '../helpers';
+import { METRIX, OBJECT, TOAST } from '../helpers';
 import { PublicNavigatorList } from '../navigation/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { action, clean } from '../redux/Action';
+import { AppState } from '../redux';
 
 interface ILogin {
     navigation: StackNavigationProp<PublicNavigatorList, 'Login'>;
 }
 
 const Login = ({ navigation }: ILogin) => {
-    const [user, setUser] = useState({ name: '', email: '', password: '' });
+    const [user, setUser] = useState({ email: '', password: '' });
+    const dispatch = useDispatch();
 
-    
+    const { loading, error } = useSelector((state: AppState) => state.user)
+
+    const submit = () => dispatch(
+        action(
+            {
+                method: 'post',
+                type: 'LOGIN',
+                data: user,
+                path: '/auth/login'
+            }
+        ))
+
+    useEffect((): any => {
+        return () => dispatch(clean({
+            type: 'LOGIN',
+        }))
+    }, [])
+
     return (
         <Layout>
             <>
+                {error && <TOAST message={error} type={'danger'} />}
                 <View style={
                     METRIX.style('w-full py-6 px-4')}>
                     <Input
-                        placeholder="Names"
-                        handleChange={(name) => setUser(OBJECT.UPDATE(user, { name }))}
-                        returnKeyType='next'
-                        styles={
-                            METRIX.style('w-full')}
-                        value={user.name}
-                        keyboardType='default'
-                    />
-                    <Input
                         placeholder="Email"
-                        handleChange={(email) =>
-                            setUser(OBJECT.UPDATE(user, { email }))
-                        }
+                        handleChange={(email) => setUser(OBJECT.UPDATE(user, { email }))}
                         returnKeyType='next'
                         styles={
                             METRIX.style('w-full')}
                         value={user.email}
                         keyboardType='default'
                     />
+                    <Input
+                        placeholder="Password"
+                        secureTextEntry
+                        handleChange={(password) =>
+                            setUser(OBJECT.UPDATE(user, { password }))
+                        }
+                        returnKeyType='next'
+                        styles={
+                            METRIX.style('w-full')}
+                        value={user.password}
+                        keyboardType='default'
+                    />
                 </View>
                 <View style={METRIX.style('w-full px-4 py-5')}>
                     <Button
-                        handleChange={() => null}
+                        handleChange={submit}
+                        loading={loading}
                         value={'Login'}
                     />
                 </View>
