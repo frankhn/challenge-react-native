e4c79134a652d0eab6b4268239bcb486484b2417
frankhn <harfrank2@gmail.com>
2021-12-10 10:54:51 +0200

    implements signup

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,25 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StackNavigationProp } from '@react-navigation/stack';
 import { View } from 'react-native'
 import Button from '../components/common/Button';
 import Input from '../components/common/Input';
 import Layout from '../components/Layout';
-import { METRIX } from '../helpers';
+import { METRIX, OBJECT, TOAST } from '../helpers';
 import { PublicNavigatorList } from '../navigation/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { action, clean } from '../redux/Action';
+import { AppState } from '../redux';
 
-interface ISignup{
+interface ISignup {
     navigation: StackNavigationProp<PublicNavigatorList, 'Signup'>;
 }
 
 const Signup = ({ navigation }: ISignup) => {
+    const [user, setUser] = useState({ name: '', email: '', password: '' });
+    const dispatch = useDispatch();
+
+    const { loading, error, data } = useSelector((state: AppState) => state.user)
+
+    const submit = () => dispatch(
+        action(
+            {
+                method: 'post',
+                type: 'SIGNUP',
+                data: {
+                    first_name: user.name,
+                    last_name: user.name,
+                    gender: 'male',
+                    password: user.password,
+                    email: user.email,
+                    phone_number: `25078888${Math.floor(1000 + Math.random() * 9000)}`,
+                    username: user.name
+                },
+                path: '/auth/signup'
+            }
+        ))
+
+    useEffect((): any => {
+        return () => dispatch(clean({
+            type: 'LOGIN',
+        }))
+    }, [])
+
     return (
         <Layout>
             <>
+                {error && <TOAST message={error} type={'danger'} />}
+                {data && <TOAST message={data.message} type={'success'} />}
                 <View style={
                     METRIX.style('w-full py-6 px-4')}>
                     <Input
                         placeholder="Names"
-                        handleChange={() => null}
+                        handleChange={(name) =>
+                            setUser(OBJECT.UPDATE(user, { name }))
+                        }
                         returnKeyType='next'
                         styles={
                             METRIX.style('w-full')}
@@ -28,11 +64,25 @@ const Signup = ({ navigation }: ISignup) => {
                     />
                     <Input
                         placeholder="Email"
-                        handleChange={() => null}
+                        handleChange={(email) =>
+                            setUser(OBJECT.UPDATE(user, { email }))
+                        }
                         returnKeyType='next'
                         styles={
                             METRIX.style('w-full')}
                         value=''
+                        keyboardType='default'
+                    />
+                    <Input
+                        placeholder="Password"
+                        secureTextEntry
+                        handleChange={(password) =>
+                            setUser(OBJECT.UPDATE(user, { password }))
+                        }
+                        returnKeyType='next'
+                        styles={
+                            METRIX.style('w-full')}
+                        value={user.password}
                         keyboardType='default'
                     />
                     <View style={METRIX.style('flex-row justify-between')}>
@@ -57,7 +107,8 @@ const Signup = ({ navigation }: ISignup) => {
                 </View>
                 <View style={METRIX.style('w-full px-4 py-10')}>
                     <Button
-                        handleChange={() => null}
+                        loading={loading}
+                        handleChange={submit}
                         value={'Create Account'}
                     />
                 </View>
